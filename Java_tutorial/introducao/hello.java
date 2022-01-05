f4fadbf3431610983f902b7227116e5a010711a1
Lucas Carvalho <lucascarvalholc2001@gmail.com>
2022-01-05 11:34:16 -0300

    Primeiro commit

---
+++ Java_tutorial/introducao/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+
+        //System é uma classe global da aplicação. 
+        System.out.println("Hello world");
+    }
+}
---
+++ Java_tutorial/introducao/variables.java
@@ -0,0 +1,15 @@
+//Java possui tipagem estática, não aceitando de tipos não correspondentes.
+
+public class variables {
+    public static void main (String[] args) {
+
+        //Estes são os tipos primitivos disponíveis
+
+        int age = 19;
+        String nome = "Lucas";
+        boolean algo = true;
+        double salario = 1500.00;
+        char classe = 'A';
+        
+    } 
+}
